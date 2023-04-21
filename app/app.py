19619ce69fcf17912f2fc93d2494c4dae7a34f73
Aniket Acharya <79405735+AniketAch@users.noreply.github.com>
2023-04-21 22:52:29 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,10 +7,10 @@ import subprocess
 
 app = Flask(__name__)
 
-# user = 'username'        # username as set for the mongodb admin server (the username used in secret.yaml - before base64 conversion)
-# password ='password'      # password as set for the mongodb admin server (the password used in secret.yaml - before base64 conversion)
-# host = 'mongodb-service'    # service name of the mongodb admin server as set in the service for mongodb server
-# port =  '27017'           # port number of the mongodb admin server as set in the service for mongodb server
+user = 'username'        # username as set for the mongodb admin server (the username used in secret.yaml - before base64 conversion)
+password ='password'      # password as set for the mongodb admin server (the password used in secret.yaml - before base64 conversion)
+host = 'mongodb-service'    # service name of the mongodb admin server as set in the service for mongodb server
+port =  '27017'           # port number of the mongodb admin server as set in the service for mongodb server
 conn_string = f'mongodb://{user}:{password}@{host}:{port}'
 db = MongoClient(conn_string).blog
 
